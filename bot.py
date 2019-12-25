aa20154ed83d8efe52c869761477d5ae7708e7f9
lathan1623 <lathan1623@me.com>
2019-12-25 17:48:32 -0600

    initial commit

---
+++ bot.py
@@ -0,0 +1,38 @@
+from twython import Twython
+import random
+import time
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+twitter = Twython (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+viewed = []
+while True:
+    try:
+         results = twitter.cursor(twitter.search, q="maxh76")
+         for result in results:
+             name = result['user']
+             screen_name = name['screen_name']
+             tweet = result['text']
+             id = result['id']
+             if id not in viewed:
+                 print(tweet)
+                 viewed.append(id)
+                 messages = ["max bad!", "max bad.", "max bad?", "max bad!"]
+                 message = random.choice(messages) + " @" + screen_name
+                 result = twitter.update_status(status=message, in_reply_to_status_id=id)
+                 print(message)
+    except:
+        print("no more tweets")
+        time.sleep(30)
+
